4d6b1d8f3bd9e4f932d2e6210f780e37b98511a7
fulviocanducci <fulviocanducci@hotmail.com>
2017-10-04 13:51:24 -0300

    All

---
+++ Canducci.Console/Program.cs
@@ -0,0 +1,44 @@
+﻿using Canducci.QueryBuilder.Dapper.SQLServer;
+using System;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace Canducci.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (IDbConnection connection =
+                            new SqlConnection("Server=.\\SQLExpress;Database=Test;User Id=sa;Password=senha;"))
+            {
+                //var a1 = connection.Build()
+                //    .From("Pessoas")
+                //    .Insert(new string[] { "Nome" }, new object[] { "Zeze de Souza" })
+                //    .SaveOrUpdate();
+
+                var a2 = connection.Build()
+                    .From("Pessoas")
+                    .Update(new string[] { "Nome" }, new object[] { "Hugo Leonardo Canducci Dias" })
+                    .Where("Id", 10)
+                    .SaveOrUpdate();
+
+
+
+                var result = connection.Build()
+                    .Select("*")
+                    .From("Pessoas")
+                    .Where(q => q.Where("Id", ">", 0).Where("Id", "<>", 0))
+                    .OrderBy("Nome")
+                    .List<Pessoa>();
+
+                foreach(var item in result)
+                {
+                    System.Console.WriteLine("{0:000} - {1}", item.Id, item.Nome);
+                }
+            }
+
+            System.Console.ReadKey();
+        }
+    }
+}
